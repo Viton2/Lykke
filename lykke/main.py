afe515a0cd059847f5e6b9332b21acb1f2fe8b74
vitinhocastro2@gmail.com <vitinhocastro2@gmail.com>
2022-06-11 20:38:48 -0300

    Tabelas novas

--- lykke/main.py
+++ lykke/main.py
@@ -57,9 +57,45 @@ def get_contact():
     return render_template('get_contact.html', msg=msg)
 
 
+@app.route('/graficobarra')
+def barras():
+    mysql = SQL()
+    comando = "SELECT intervalo, proporcao FROM autoavaliacao_saude"
+
+    cs = mysql.consultar(comando, ())
+    grf = ""
+    for [intervalo, proporcao] in cs:
+        grf += f", ['{intervalo}', {proporcao}, '#9999FF']"
+    cs.close()
+
+    return render_template('graficobarra.html', barras=grf)
+
+
 @app.route('/conteudo')
 def conteudo():
     return render_template('conteudo.html')
+
+
+@app.route('/doisgraficos')
+def barras_02():
+    mysql = SQL()
+    comando = "SELECT intervalo, proporcao FROM consumo_hortalicas"
+
+    cs = mysql.consultar(comando, ())
+    grf = ""
+    for [intervalo, proporcao] in cs:
+        grf += f", ['{intervalo}', {proporcao}, '#F59443']"
+    cs.close()
+
+    comando_02 = "SELECT intervalo, proporcao FROM consumo_feijao"
+
+    cs_02 = mysql.consultar(comando_02, ())
+    grf_02 = ""
+    for [intervalo, proporcao] in cs_02:
+        grf_02 += f", ['{intervalo}', {proporcao}, '#00D4F7']"
+    cs_02.close()
+
+    return render_template('doisgraficos.html', barras_01=grf, barras_02=grf_02)
 
 
 @app.route('/sobre')
